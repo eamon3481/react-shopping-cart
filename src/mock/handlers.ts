e409f13d29834ef380fd1e79d4cb274c37feaa47
Dongmin Ahn <eamon3481@naver.com>
2023-03-14 14:39:55 +0900

    chore : add msw handler
    - GET : /cart
    - POST : /cart
    - DELETE : /cart
    - GET : orders
    - GET : orders/:id
    - POST : orders
    - DELETE : orders

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -2,7 +2,7 @@ import { rest } from 'msw';
 import db from './db.json';
 import { isProduct } from './utils';
 
-const { products } = db;
+const { products, carts, orders } = db;
 
 export const handlers = [
   rest.get('/products', (req, res, ctx) => {
@@ -15,12 +15,63 @@ export const handlers = [
       return res(ctx.status(404), ctx.json({ message: 'Product not found' }));
     return res(ctx.status(200), ctx.json(product));
   }),
-  rest.post('/products', (req, res, ctx) => {
-    const product = req.body;
+  rest.post('/products', async (req, res, ctx) => {
+    const product = await req.json();
     if (!isProduct(product))
       return res(ctx.status(400), ctx.json({ message: 'Invalid product' }));
 
     products.push(product);
     return res(ctx.status(201), ctx.json(product));
+  }),
+
+  rest.get('/cart', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(carts));
+  }),
+  rest.post('/cart', async (req, res, ctx) => {
+    const product = await req.json();
+    if (!isProduct(product))
+      return res(ctx.status(400), ctx.json({ message: 'Invalid cart' }));
+    carts.push({
+      id: carts.length + 1,
+      product,
+    });
+    return res(ctx.status(201), ctx.json(carts));
+  }),
+  rest.delete('/cart/:id', (req, res, ctx) => {
+    const { id } = req.params;
+    const cart = carts.find((c) => c.id === Number(id));
+    if (!cart)
+      return res(ctx.status(404), ctx.json({ message: 'Cart not found' }));
+    carts.splice(carts.indexOf(cart), 1);
+    return res(ctx.status(200), ctx.json(cart));
+  }),
+
+  rest.get('/orders', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(orders));
+  }),
+
+  rest.get('/orders/:id', (req, res, ctx) => {
+    const { id } = req.params;
+    const order = orders.find((o) => o.id === Number(id));
+    if (!order)
+      return res(ctx.status(404), ctx.json({ message: 'Order not found' }));
+    return res(ctx.status(200), ctx.json(order));
+  }),
+
+  rest.post('/orders', async (req, res, ctx) => {
+    const order = await req.json();
+    if (!order)
+      return res(ctx.status(400), ctx.json({ message: 'Invalid order' }));
+    orders.push(order);
+    return res(ctx.status(201), ctx.json(order));
+  }),
+
+  rest.delete('/orders/:id', (req, res, ctx) => {
+    const { id } = req.params;
+    const order = orders.find((o) => o.id === Number(id));
+    if (!order)
+      return res(ctx.status(404), ctx.json({ message: 'Order not found' }));
+    orders.splice(orders.indexOf(order), 1);
+    return res(ctx.status(200), ctx.json(order));
   }),
 ];
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,20 @@
-interface Product {
+export interface Product {
   id: number;
   name: string;
   price: number;
   imageUrl: string;
+}
+
+export interface Cart {
+  id: number;
+  product: Product;
+}
+
+export interface Order {
+  id: number;
+  orderDetails: OrderDetail[];
+}
+
+export interface OrderDetail extends Product {
+  quantity: number;
 }
