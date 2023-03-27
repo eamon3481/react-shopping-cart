3abdc157cd57b547aeff12e9042634ebb6256812
Dongmin Ahn <eamon3481@naver.com>
2023-03-27 22:32:10 +0900

    feat : add GlobalStyle

---
+++ src/style/GlobalStyle.tsx
@@ -0,0 +1,50 @@
+import { css, Global } from '@emotion/react';
+
+const globalStyleCss = css`
+  * {
+    box-sizing: border-box;
+  }
+  html {
+    height: 100%;
+  }
+
+  li,
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  button {
+    background-color: transparent;
+    border: none;
+    outline: none;
+    cursor: pointer;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  input {
+    font-size: 16px;
+
+    appearance: none;
+    border: none;
+    outline: none;
+  }
+
+  #root {
+    height: 100%;
+  }
+
+  body {
+    height: 100%;
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const GlobalStyle = () => <Global styles={globalStyleCss} />;
+
+export default GlobalStyle;
